6dfe983f02483fe5ef0cbf115242380a033df82a
Jakub Ciepły <jakubc01@gmail.com>
2021-04-03 15:24:19 +0200

    Add main view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-const App = () => (
-  <div className="App">
-    Hello world
-  </div>
-);
+import { CssBaseline, Typography } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useState } from 'react';
+import Search from './components/Search';
+
+const App = () => {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+  const [location, setLocation] = useState(null);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <img src="/images/space.jpg" alt="" className="background" />
+        <Typography variant="h1" align="center">Satellite</Typography>
+        <Search location={location} setLocation={setLocation} />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
